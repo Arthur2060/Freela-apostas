52c11d9091a9e5b2e6e7c619b5d1b2aa35b86302
Arthur <arthurhenrrique1705@gmail.com>
2026-02-11 19:13:46 -0300

    Primeira versão do CRUD de apostas adicionado

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const serviceAccount = require('./freela-apostas-firebase-adminsdk-fbsvc-89af620
 // Inicialização do Firebase Admin SDK
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-} );
+});
 
 console.log('Firebase Admin SDK inicializado com sucesso!');
 
@@ -149,6 +149,107 @@ app.delete('/jogadores/:id', async (req, res) => {
     } catch (error) {
         console.error('Erro ao deletar jogador:', error);
         res.status(500).json({ error: 'Erro ao deletar jogador' });
+    }
+});
+
+// Endpoints para apostas
+
+app.get('/apostas', async (req, res) => {
+    try {
+        const apostasSnapshot = await db.collection('apostas').get();
+        const apostas = apostasSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        res.json(apostas);
+    } catch (error) {
+        console.error('Erro ao buscar apostas:', error);
+        res.status(500).json({ error: 'Erro ao buscar apostas' });
+    }
+});
+
+app.post('/apostas', async (req, res) => {
+    try {
+        const { primeiroJogador, segundoJogador, validade } = req.body;
+
+        const tabelaOdds = {
+            0:  { v1: 3.0, v2: 3.0, emp: 4.0 },
+            1:  { v1: 4.0, v2: 2.0, emp: 3.0 },
+            2:  { v1: 5.0, v2: 1.5, emp: 4.0 },
+            3:  { v1: 5.0, v2: 1.5, emp: 6.0 },
+            4:  { v1: 6.0, v2: 1.3, emp: 7.0 }
+        }
+
+        switch (Math.abs(primeiroJogador.nivel - segundoJogador.nivel)) {
+            case 0:
+                oddVitoriaPrimeiro = tabelaOdds[0].v1;
+                oddVitoriaSegundo = tabelaOdds[0].v2;
+                oddEmpate = tabelaOdds[0].emp;
+                break;
+            case 1:
+                oddVitoriaPrimeiro = tabelaOdds[1].v1;
+                oddVitoriaSegundo = tabelaOdds[1].v2;
+                oddEmpate = tabelaOdds[1].emp;
+                break;
+            case 2:
+                oddVitoriaPrimeiro = tabelaOdds[2].v1;
+                oddVitoriaSegundo = tabelaOdds[2].v2;
+                oddEmpate = tabelaOdds[2].emp;
+                break;
+            case 3:
+                oddVitoriaPrimeiro = tabelaOdds[3].v1;
+                oddVitoriaSegundo = tabelaOdds[3].v2;
+                oddEmpate = tabelaOdds[3].emp;
+                break;
+            case 4:
+                oddVitoriaPrimeiro = tabelaOdds[4].v1;
+                oddVitoriaSegundo = tabelaOdds[4].v2;
+                oddEmpate = tabelaOdds[4].emp;
+        }
+
+        const resp = await db.collection(`apostas`).add({
+            primeiroJogador,
+            segundoJogador,
+            oddVitoriaPrimeiro,
+            oddVitoriaSegundo,
+            oddEmpate,
+            validade,
+            resultado: null
+        });
+        res.json({ id: resp.id, primeiroJogador, segundoJogador, oddVitoriaPrimeiro, oddVitoriaSegundo, oddEmpate, validade, resultado: null });
+    } catch (error) {
+        console.error('Erro ao adicionar aposta:', error);
+        res.status(500).json({ error: 'Erro ao adicionar aposta' });
+    }
+})
+
+app.put('/apostas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { primeiroJogador, segundoJogador, oddVitoriaPrimeiro, oddVitoriaSegundo, oddEmpate, validade, resultado } = req.body;
+
+        await db.collection('apostas').doc(id).set({
+            primeiroJogador: primeiroJogador,
+            segundoJogador: segundoJogador,
+            oddVitoriaPrimeiro: oddVitoriaPrimeiro,
+            oddVitoriaSegundo: oddVitoriaSegundo,
+            oddEmpate: oddEmpate,
+            validade: validade,
+            resultado: resultado
+        });
+
+        res.json({ id, primeiroJogador, segundoJogador, oddVitoriaPrimeiro, oddVitoriaSegundo, oddEmpate, validade, resultado });
+    } catch (error) {
+        console.error('Erro ao atualizar aposta:', error);
+        res.status(500).json({ error: 'Erro ao atualizar aposta' });
+    }
+});
+
+app.delete('/apostas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await db.collection('apostas').doc(id).delete();
+        res.json({ message: 'Aposta deletada com sucesso' });
+    } catch (error) {
+        console.error('Erro ao deletar aposta:', error);
+        res.status(500).json({ error: 'Erro ao deletar aposta' });
     }
 });
 
