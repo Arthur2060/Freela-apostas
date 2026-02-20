3cf427befe14d48228060c7339a3a1a1041c420b
Arthur <arthurhenrrique1705@gmail.com>
2026-02-20 13:12:15 -0300

    COncertando url do firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const firebase_cert = require("../etc/secrets/firebase-admin.json") // Lembrar de adicionar json ao voltar para casa
+const firebase_cert = require("/etc/secrets/firebase-admin.json") // Lembrar de adicionar json ao voltar para casa
 
 admin.initializeApp({
     credential: admin.credential.cert(firebase_cert),
