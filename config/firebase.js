3b30bc8c14e0541fcc4576e255c730aaf9a78703
Arthur <arthurhenrrique1705@gmail.com>
2026-02-20 16:08:37 -0300

    Trocando firebase-credential

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const firebase_cert = require("./freela-apostas-firebase-adminsdk-fbsvc-89af620e4c.json") // Lembrar de adicionar json ao voltar para casa
+const firebase_cert = require("/etc/secrets/firebase-credential") // Lembrar de adicionar json ao voltar para casa
 
 admin.initializeApp({
     credential: admin.credential.cert(firebase_cert),
