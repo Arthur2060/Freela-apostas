b6dafd0d1650a7d92f12e5b32646fd5096987d09
Arthur <arthurhenrrique1705@gmail.com>
2026-02-20 17:54:22 -0300

    Concertando chamada de credencial

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const firebase_cert = require("./freela-apostas-firebase-adminsdk-fbsvc-d7e059a819.json") // Lembrar de adicionar json ao voltar para casa
+const firebase_cert = require("/etc/secrets/firebase-credential.json") // Lembrar de adicionar json ao voltar para casa
 
 admin.initializeApp({
     credential: admin.credential.cert(firebase_cert),
