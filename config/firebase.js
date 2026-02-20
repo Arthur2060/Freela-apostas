aff41128a71741a08eade69113784ed222f36d3b
Arthur <arthurhenrrique1705@gmail.com>
2026-02-20 15:51:04 -0300

    Isolando lib do firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,8 @@
-const admin = require('firebase-admin');
+const { initializeApp } = require('firebase-admin');
 
 const firebase_cert = require("/etc/secrets/firebase-admin.json") // Lembrar de adicionar json ao voltar para casa
 
-admin.initializeApp({
+initializeApp({
     credential: admin.credential.cert(firebase_cert),
 });
 
